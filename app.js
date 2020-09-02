bd2b56dd7368a09121434e4943e75ea5d49599ad
Brian Rudolph <brian.rudolph303@gmail.com>
2020-09-02 11:25:28 -0600

    MongoDB added to store posts

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ const ejs = require("ejs");
 const lodash = require("lodash");
 const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to my blog site. This site has been developed using Node.js - express.js - EJS - mongoDB";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const posts = [];
 const app = express();
+
+/**************** MongoDB stuff and things ******************/
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const PostSchema = {
+  title: {
+    type : String,
+    required: [true, "Your Post needs to have a title"]
+  },
+  post: {
+    type: String,
+    required: [true, "What's a post without any content, not a post. Please enter content"]
+  }
+};
+
+const Post = mongoose.model("Post", PostSchema);
+
+const dummyPost = new Post({
+  title: "No Posts Yet",
+  post: "This is just a placeholder post. Why dont you try composing your own post"
+});
+
+/**************** END --> MongoDB stuff and things ******************/
 
 app.set('view engine', 'ejs');
 
@@ -19,11 +46,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+/******** All the get requests ************/
+
 app.get("/", function(req, res){
 
-  res.render("home", {homeContent: homeStartingContent, Posts: posts});
+  Post.find({}, function(error, foundPosts){
+    if(error){
+      console.log(error);
+    } else {
+      res.render("home", { homeContent: homeStartingContent, Posts: foundPosts });
+    }
+  });
+  
 });
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
 app.get("/about", function(req, res){
   res.render("about", {content: aboutContent});
@@ -35,33 +74,39 @@ app.get("/contact", function(req, res){
 
 /* URL management */
 app.get("/posts/:postTitle", function(req,res){
-  const aTitle = lodash.lowerCase(req.params.postTitle);
-  posts.forEach(function(post){
-    const storedPostTitle = lodash.lowerCase(post.title);
-    if(storedPostTitle === aTitle){
-      res.render("post", {title: post.title, body: post.body});
-    } 
-  })
+  const aTitle = req.params.postTitle;
+  console.log(aTitle);
+  Post.findOne({title: aTitle}, function(error, foundPost){
+    const storedPostTitle = lodash.lowerCase(foundPost.title);
+    if(error) {
+      console.log(error);
+    } else {
+        res.render("post", { title: foundPost.title, body: foundPost.post });
+    }
+  });
 
 });
 
+/******** END All the get requests ************/
 
 /* Composing */
 app.post("/compose", function(req, res){
+  // const post = {
+  //   title: req.body.postTitle,
+  //   body: req.body.postText
+  // }
+  //posts.push(post);
 
-  let post = {
+  const post = new Post({
     title: req.body.postTitle,
-    body: req.body.postText
-  }
+    post: req.body.postText,
+  });
 
-  posts.push(post);
+  post.save();
   res.redirect("/");
-
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
+
 
 
 /* Function set */
